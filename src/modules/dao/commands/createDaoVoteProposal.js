473a0a67d5d3fa08488deaea6e1507d2a4d0ccc7
Marcia Ong <MrMarciaOng@gmail.com>
2025-06-26 00:23:58 +0800

    fix import issue

--- src/modules/dao/commands/createDaoVoteProposal.js
+++ src/modules/dao/commands/createDaoVoteProposal.js
@@ -1,44 +1,54 @@
-import { getNameForReply } from "../../../common/getNameForReply";
-import { MINIMUM_APPROVAL_PERCENTAGE, MINIMUM_REQUIRED_VOTES, TWENTY_FOUR_HOURS } from "../../../constants/constants";
-import { closeDaoVoteProposal } from "./closeDaoVoteProposal";
+const { getNameForReply } = require("../../../common/getNameForReply");
+const {
+  MINIMUM_APPROVAL_PERCENTAGE,
+  MINIMUM_REQUIRED_VOTES,
+  TWENTY_FOUR_HOURS,
+} = require("../../../constants/constants");
+const { closeDaoVoteProposal } = require("./closeDaoVoteProposal");
 
-export async function createDaoVoteProposal(bot, message, match) {
-    const messageId = message.message_id;
-    const messageThreadId = message.message_thread_id;
-    const chatId = message.chat.id;
-    const usernameOfProposer = getNameForReply(msg);
+async function createDaoVoteProposal(bot, message, match) {
+  const messageId = message.message_id;
+  const messageThreadId = message.message_thread_id;
+  const chatId = message.chat.id;
+  const usernameOfProposer = getNameForReply(message);
 
-    const proposedPollTitle = match[1];  
+  const proposedPollTitle = match[1];
 
-    const messageToSend = generateDaoVoteProposalMessage(
-        proposedPollTitle,
-        usernameOfProposer,
-    )
+  const messageToSend = generateDaoVoteProposalMessage(
+    proposedPollTitle,
+    usernameOfProposer
+  );
 
-    const { poll } = await bot.sendPoll(
-        chatId,
-        messageToSend,
-        ["Approve", "Deny"],
-        {
-            message_thread_id: messageThreadId,
-            reply_to_message_id: messageId,
-        }
-    );
+  const { poll } = await bot.sendPoll(
+    chatId,
+    messageToSend,
+    ["Approve", "Deny"],
+    {
+      message_thread_id: messageThreadId,
+      reply_to_message_id: messageId,
+    }
+  );
 
-    setTimeout(() => {
-        closeDaoVoteProposal(bot, chatId, messageThreadId, poll.id);
-    }, TWENTY_FOUR_HOURS);
-};
+  setTimeout(() => {
+    closeDaoVoteProposal(bot, chatId, messageThreadId, poll.id);
+  }, TWENTY_FOUR_HOURS);
+}
 
 function generateDaoVoteProposalMessage(proposedPollTitle, usernameOfProposer) {
-    const message = `
+  const message = `
 ${usernameOfProposer} is currently proposing to create a DAO Vote with:
 
 ${proposedPollTitle}
 
-A minimum of ${MINIMUM_REQUIRED_VOTES} votes and an approval rate of ${MINIMUM_APPROVAL_PERCENTAGE * 100}% is required for the proposal to pass.
+A minimum of ${MINIMUM_REQUIRED_VOTES} votes and an approval rate of ${
+    MINIMUM_APPROVAL_PERCENTAGE * 100
+  }% is required for the proposal to pass.
 The poll will be open for 24 hours.
-    `
+    `;
 
-    return message;
+  return message;
 }
+
+module.exports = {
+  createDaoVoteProposal,
+};
